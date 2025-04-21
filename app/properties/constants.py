79c0a7bccbf10f38072489686db943ee99e7448e
klsa2921 <140685163+klsa2921@users.noreply.github.com>
2025-04-21 16:36:08 +0400

    updated constants

--- app/properties/constants.py
+++ app/properties/constants.py
@@ -7,22 +7,25 @@
 #     VESPA_APPLICATION_PATH="/app/vespa/"
 #     STATIC_WEB_DIRECTORY="/app/web/"
 
-# class env:
-#     VESPA_INDEX_SEARCH_URL = "http://192.168.1.27:2923"
-#     VESPA_APPLICATION_URL="http://192.168.1.27:2922/application/v2"
-#     MODEL_NAME="all-MiniLM-L6-v2"
-#     INDEX_HTML_PATH="C:/Users/mmallikanti/Documents/GitHub/vespa/app/web/index.html"
-#     FILE_UPLOAD_DIRECTORY="C:/Users/mmallikanti/Documents/GitHub/vespa/app/uploads/"
-#     VESPA_APPLICATION_PATH="C:/Users/mmallikanti/Documents/GitHub/vespa/app/docker/app-text"
-#     STATIC_WEB_DIRECTORY="C:/Users/mmallikanti/Documents/GitHub/vespa/app/web/"
-
-
-
 class env:
-    VESPA_INDEX_SEARCH_URL = "http://localhost:2923"
-    VESPA_APPLICATION_URL="http://localhost:2922/application/v2"
+    VESPA_INDEX_SEARCH_URL = "http://192.168.1.27:2923"
+    VESPA_APPLICATION_URL="http://192.168.1.27:2922/application/v2"
     MODEL_NAME="all-MiniLM-L6-v2"
-    INDEX_HTML_PATH="/home/klsa/Documents/GitHub/vespa/app/web/index.html"
-    FILE_UPLOAD_DIRECTORY="/home/klsa/Documents/GitHub/vespa/app/uploads/"
-    VESPA_APPLICATION_PATH="/home/klsa/Documents/GitHub/vespa/app/docker/app-text"
-    STATIC_WEB_DIRECTORY="/home/klsa/Documents/GitHub/vespa/app/web/"
+    INDEX_HTML_PATH="C:/Users/mmallikanti/Documents/GitHub/vespa/app/web/index.html"
+    FILE_UPLOAD_DIRECTORY="C:/Users/mmallikanti/Documents/GitHub/vespa/app/uploads/"
+    VESPA_APPLICATION_PATH="C:/Users/mmallikanti/Documents/GitHub/vespa/app/docker/app-text"
+    STATIC_WEB_DIRECTORY="C:/Users/mmallikanti/Documents/GitHub/vespa/app/web/"
+    CHAT_MODEL_API_URL="http://192.168.1.6:3009"
+    CHAT_MODEL_NAME="llama3.2:11b"
+    QA_INDEX_NAME="qa_index"
+
+
+
+# class env:
+#     VESPA_INDEX_SEARCH_URL = "http://localhost:2923"
+#     VESPA_APPLICATION_URL="http://localhost:2922/application/v2"
+#     MODEL_NAME="all-MiniLM-L6-v2"
+#     INDEX_HTML_PATH="/home/klsa/Documents/GitHub/vespa/app/web/index.html"
+#     FILE_UPLOAD_DIRECTORY="/home/klsa/Documents/GitHub/vespa/app/uploads/"
+#     VESPA_APPLICATION_PATH="/home/klsa/Documents/GitHub/vespa/app/docker/app-text"
+#     STATIC_WEB_DIRECTORY="/home/klsa/Documents/GitHub/vespa/app/web/"
